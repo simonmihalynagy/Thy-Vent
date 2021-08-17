59dc39cf0567eef7afce68d557821b35f8e1d7a9
Simon Nagy <nagysimonmihaly@gmail.com>
2021-08-17 21:37:54 +0200

    fixed all that we dont understand, we still dont understand but it works, good stuff

--- config/session-config.js
+++ config/session-config.js
@@ -1,23 +1,22 @@
-const session = require('express-session');
+const session = require("express-session");
 
 // ADDED: require mongostore
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 // ADDED: require mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = (app) => {
   app.use(
     session({
-      secret: process.env.SESS_SECRET,
+      secret: "whatever",
       resave: false,
       saveUninitialized: true,
       cookie: { maxAge: 60000 },
       store: MongoStore.create({
         // <== ADDED !!!
         mongoUrl:
-          process.env.MONGODB_URI ||
-          'mongodb://localhost/lab-express-basic-auth',
+          process.env.MONGODB_URI || "mongodb://localhost/event-project",
         // ttl => time to live
         ttl: 10, // 60sec * 60min * 24h => 1 day
       }),
