12cb184e5f4cd2bbf4c1b81ed69cac75c78e73da
Hannes Polte <hannes.polte@gmx.de>
2021-09-01 14:13:07 +0200

    changed some design, added buttons to move between pages, worked on the seed file

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  if (req.session.currentUser) {
+    res.redirect(`/users/${req.session.currentUser}`);
+  }
   res.render("index", { title: "Event App" });
+});
+router.get("/signout", (req, res) => {
+  console.log(req.session.currentUser);
+  delete req.session.currentUser;
+  res.redirect("/");
 });
 
 module.exports = router;
